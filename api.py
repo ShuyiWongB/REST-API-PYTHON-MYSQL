13dba79cec3c3eb7a2f65c310de60dd197dec136
ShuyiWongB <63923001+ShuyiWongB@users.noreply.github.com>
2021-08-12 21:15:35 -0400

    Update api.py

--- api.py
+++ api.py
@@ -31,7 +31,8 @@ users = {
 #Autentificador
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
+    if username in users and 
+check_password_hash(users.get(username), password):
         return username
     else:
         abort(401)
